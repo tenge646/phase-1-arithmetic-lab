96e2a5c0181e8f758cf927f6a7199d391df37498
kinsman646 <teesam2004@gmail.com>
2023-09-21 09:57:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+function RandomInteger() {
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+  }
+  
+  const random = RandomInteger();
+  console.log(random);
+
+
+ 
+let num3 = 40; 
+let num4 = 10;
+
+let mod = (num3 % num4) + 4;
+
+console.log(mod);
+
+
+
+const numbers = [10, 15, 5, 20, 8];
+
+const max = Math.max(...numbers);
+
+console.log(max); 
+
